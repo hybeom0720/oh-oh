65fcd92f45028fec8db365b4c49d1920d1cddca1
최상하 <sanghac19@likelion.org>
2020-11-15 19:29:19 +0900

    [fix]: page 디렉토리 삭제

--- client/src/Pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Intro } from "./Intro";
-export { default as MainMap } from "./MainMap";
-export { default as SearchMap } from "./SearchMap";
-export { default as Report } from "./Report";
-export { default as Detail } from "./Detail";
-export { default as Error } from "./Error";
